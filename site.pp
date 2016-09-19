c5f43f29732a06b5a33af2ca493658d291029416
root <root@agent1.acme.com>
2016-09-19 14:21:50 +0000

    updated site.pp (for apache) and files directory

--- site.pp
+++ site.pp
@@ -1,10 +1,20 @@
-user { 'praveen':
-  ensure           => 'present',
-  gid              => '1001',
-  home             => '/home/praveen',
-  password         => '$6$x.wAdHjS$iN0V6x9FkeFWPJcvO5WXOw2OqShCW/CCXFcTPA.zqhZZg8BqI4MFvkfNWp9gD0giHQuF1dBZvUe0Sl0nxhnnq1',
-  password_max_age => '99999',
-  password_min_age => '0',
-  shell            => '/bin/bash',
-  uid              => '1001',
+package { 'apache2': 
+	ensure => present,
+}
+
+file { '/var/www':
+	ensure => directory,
+}
+
+file { '/var/www/html':
+        ensure => directory,
+}
+
+file { '/var/www/html/index.html':
+ensure => file,
+source => '/etc/puppetlabs/puppet/files/index.html',
+}
+
+service { 'apache2': 
+	ensure => running,
 }
